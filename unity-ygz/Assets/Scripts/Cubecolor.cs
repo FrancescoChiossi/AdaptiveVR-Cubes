48380b8f4678967605a083f08e62bf44c71bbe1f
Yagiz Turgut <ygztrgt@gmail.com>
2022-02-14 23:22:56 +0100

    out of bounds solved + 3 versions

---
+++ unity-ygz/Assets/Scripts/Cubecolor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubecolor : MonoBehaviour
+{
+    public Material[] materials;
+    public Material selectedColor;
+    public string cubecolor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Rotate(new Vector3(0, 90 * Time.deltaTime, 0));
+    }
+
+    /*public void setSelected()
+    {
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+
+        Material[] mats = renderer.materials;
+        mats[0] = selectedColor;
+        renderer.materials = mats;
+    }*/
+
+
+    public void reset()
+    {
+        if (materials.Length > 0)
+        {
+            Material m = materials[Random.Range(0, materials.Length)];
+            cubecolor = m.name;
+            MeshRenderer renderer = GetComponent<MeshRenderer>();
+
+            Material[] mats = renderer.materials;
+            mats[0] = m;
+            renderer.materials = mats;
+        }
+        else
+        {
+            Debug.Log("No materials for shirts set.");
+        }
+    }
+}
---
+++ unıty-ygz-better/Assets/Scripts/CountPeople.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountPeople : MonoBehaviour
+{
+    private bool isEnabled = false;
+    public int count = 0;
+    public DataLogger logger;
+
+    private double startTime = 0;
+
+    public double timeSpan = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "visitor")
+        {
+            GameObject cube = col.gameObject;
+           
+
+            var timestamp = UnixTime.GetTime();
+            timeSpan = timestamp - startTime;
+          
+            //string shirtcolor = cube.GetComponent<ShirtController>().shirtcolor;
+            
+            logger.writeFlow(timestamp, cube.name);
+        }
+
+
+        if (col.gameObject.tag == "visitor" && isEnabled) {
+            count++;
+        }
+    }
+
+    public int getCount()
+    {
+        return count;
+    }
+
+    public double getCounterTime()
+    {
+        return startTime;
+    }
+
+    public void setCounterEnabled(bool b)
+    {
+        if (b == true)
+        {
+            count = 0;
+            startTime = UnixTime.GetTime();
+        }
+        isEnabled = b;
+    }
+}
